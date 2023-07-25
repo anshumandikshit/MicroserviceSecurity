0e5ce4af81b75193ed1f3e2c2f0d0cfdf912433e
Anshuman Dikshit <andikshit@deloitte.com>
2023-07-25 17:25:56 +0530

    Create Sample APIs with some Dummy Data

---
+++ MicroServiceSecuritySample/Product.API/Dtos/ProductsDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Product.API.Dtos
+{
+    public class Product
+    {
+        public int ProdcutId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int ProductPrice { get; set; }
+    }
+}
---
+++ MicroServiceSecuritySample/Product.API/MockData/ProductMockDatas.cs
@@ -0,0 +1,37 @@
+﻿using Product.API.Dtos;
+using System.Collections;
+
+
+
+namespace Product.API.MockData
+{
+    public class ProductMockDatas
+    {
+
+
+        public List<ProductsDto> GetProductData()
+        {
+            return new List<ProductsDto>()
+            {
+                new ProductsDto()
+                {
+                    ProdcutId =1,
+                    ProductName="Product1",
+                    ProductPrice=10,
+                },
+                new ProductsDto()
+                {
+                    ProdcutId =2,
+                    ProductName="Product2",
+                    ProductPrice=20,
+                },
+                new ProductsDto()
+                {
+                    ProdcutId =3,
+                    ProductName="Product3",
+                    ProductPrice=30,
+                }
+            };
+        }
+    }
+}
